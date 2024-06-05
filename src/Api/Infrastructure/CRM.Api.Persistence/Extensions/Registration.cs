e1f389aaddefb03803231c6149562532cbcb1704
Berfin Tek <tekberfin@outlook.com>
2024-06-05 18:20:30 +0300

    Database registration added

---
+++ src/Api/Infrastructure/CRM.Api.Persistence/Extensions/Registration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Api.Persistence.Extensions;
+
+public static class Registration
+{
+    public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DbContext>(conf =>
+        {
+            var connStr = configuration["CRMDbConnectionString"].ToString();
+            conf.UseNpgsql(connStr, opt =>
+            {
+                opt.EnableRetryOnFailure();
+            });
+        });
+
+        return services;
+    }
+}
